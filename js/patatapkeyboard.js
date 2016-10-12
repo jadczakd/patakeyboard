ee3f7c6bdf68169c24b5eda8412160a1aa99771c
jadczakd <jadczak.d@gmail.com>
2016-10-12 11:51:16 +0200

    minor changes

--- js/patatapkeyboard.js
+++ js/patatapkeyboard.js
@@ -1,15 +1,15 @@
-    	var currentSetI = 0;
-    	var nextSetI = 1;
-		var previousSetI = 5;
-		var set =["A","B","C","D","E","F"];
-		var bgcolors=["#FFF","#EEE","#DDD","#CCC","#BBB","#AAA"];
-        var letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-   		var generateHowl = function(target){
-  			  return "sounds/" + set[currentSetI] + "/" + keySounds[target];
-			}
-	var shape = 1; 
-	var keySounds={m:"piston-3.mp3",n:"zig-zag.mp3",b:"wipe.mp3",v:"veil.mp3",c:"ufo.mp3",x:"timer.mp3",z:"suspension.mp3",l:"strike.mp3",k:"squiggle.mp3",j:"splits.mp3",h:"prism-3.mp3",g:"prism-2.mp3",f:"prism-1.mp3",d:"piston-2.mp3",a:"pinwheel.mp3",s:"piston-1.mp3",q:"bubbles.mp3", w:"clay.mp3",e:"confetti.mp3",r:"corona.mp3",t:"dotted-spiral.mp3",y:"flash-1.mp3",u:"flash-2.mp3",i:"flash-3.mp3",o:"glimmer.mp3",p:"moon.mp3" };
-    var keysData = {
+var currentSetI = 0;
+var nextSetI = 1;
+var previousSetI = 5;
+var set =["A","B","C","D","E","F"];
+var bgcolors=["#FFF","#EEE","#DDD","#CCC","#BBB","#AAA"];
+var letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var generateHowl = function(target){
+  return "sounds/" + set[currentSetI] + "/" + keySounds[target];
+}
+var shape = 1; 
+var keySounds={m:"piston-3.mp3",n:"zig-zag.mp3",b:"wipe.mp3",v:"veil.mp3",c:"ufo.mp3",x:"timer.mp3",z:"suspension.mp3",l:"strike.mp3",k:"squiggle.mp3",j:"splits.mp3",h:"prism-3.mp3",g:"prism-2.mp3",f:"prism-1.mp3",d:"piston-2.mp3",a:"pinwheel.mp3",s:"piston-1.mp3",q:"bubbles.mp3", w:"clay.mp3",e:"confetti.mp3",r:"corona.mp3",t:"dotted-spiral.mp3",y:"flash-1.mp3",u:"flash-2.mp3",i:"flash-3.mp3",o:"glimmer.mp3",p:"moon.mp3" };
+var keysData = {
     q: {
 		sound: new Howl({
   		src: "sounds/" + set[currentSetI] + "/"+ keySounds["q"] 
@@ -168,70 +168,68 @@
 	}
 }
 
-		var nextSet = function(){
-			if(currentSetI==5){
-				currentSetI=0;
-				nextSetI=1;
-				previousSetI=5
-				setColor();
-				generateAllHowls();
-			}else{
-				currentSetI++;
-				nextSetI++;
-				previousSetI--;
-				setColor();
-				generateAllHowls();
-			}
-		}
+var nextSet = function(){
+	if(currentSetI==5){
+		currentSetI=0;
+		nextSetI=1;
+		previousSetI=5
+		setColor();
+		generateAllHowls();
+	}else{
+		currentSetI++;
+		nextSetI++;
+		previousSetI--;
+		setColor();
+		generateAllHowls();
+	}
+}
 
-		var generateAllHowls = function(letter){
-		for(key in keysData){
-			keysData[key]["sound"] = new Howl({src: generateHowl(key)})
-			}
-		}
-        var setColor = function(){
-			canv.style.background = bgcolors[currentSetI];
-		}
+var generateAllHowls = function(letter){
+	for(key in keysData){
+		keysData[key]["sound"] = new Howl({src: generateHowl(key)})
+	}
+}
+var setColor = function(){
+	canv.style.background = bgcolors[currentSetI];
+}
     	
-    	var currentCell = [ 0, 0 ];
+var currentCell = [ 0, 0 ];
 
-    	var removeActive = function(){
-    		soundTable[currentCell[0]].children[currentCell[1]].classList.remove("current");
-    	}
-		var activeCell = function(){
-			soundTable[currentCell[0]].children[currentCell[1]].classList.add("current");
-		};
+var removeActive = function(){
+	soundTable[currentCell[0]].children[currentCell[1]].classList.remove("current");
+}
+var activeCell = function(){
+	soundTable[currentCell[0]].children[currentCell[1]].classList.add("current");
+};
 
-		var canv = document.querySelector("canvas");
-		var circles = [];
-		function onKeyDown(event){
-			removeActive();
-			if(letters.indexOf(event.key)>=0){
-				var maxPoint = new Point(view.size.width, view.size.height);
-				var randomPoint = Point.random();
-				var point = maxPoint * randomPoint;
-				var randomR = Math.random()* 250+250;
-				if (shape === 1) {
+var canv = document.querySelector("canvas");
+var circles = [];
+function onKeyDown(event){
+	removeActive();
+		if(letters.indexOf(event.key)>=0){
+			var maxPoint = new Point(view.size.width, view.size.height);
+			var randomPoint = Point.random();
+			var point = maxPoint * randomPoint;
+			var randomR = Math.random()* 250+250;
+			if (shape === 1) {
 				var newCircle = new Path.RegularPolygon(point, 3, randomR);
 				shape++;	
-				}
-				else if (shape === 2) {
+			}else if (shape === 2) {
 				var newCircle = new Path.RegularPolygon(point, 4, randomR);
 				shape++;	
-				}
-				else if(shape === 3){
+			}else if(shape === 3){
 				var newCircle = new Path.RegularPolygon(point, 5, randomR);
 				shape++;
-				}else if(shape === 4){
+			}else if(shape === 4){
 				var newCircle = new Path.RegularPolygon(point, 6, randomR);
 				shape++;
-				}else if(shape === 5){
+			}else if(shape === 5){
 				var newCircle = new Path.Circle(point, randomR);
 				shape++;
-				}else if(shape === 6){
+			}else if(shape === 6){
 				var newCircle = new Path.Star(point, 10, 120, 90);
 				shape=1;
-				}
+			}
 				newCircle.fillColor = keysData[event.key].color;
 				keysData[event.key].sound.play();
 				circles.push(newCircle);
@@ -251,47 +249,46 @@
 				}
 			}else if(event.key=="left"){
 				if(currentCell[1]==0){
-					currentCell[1]=9;
+					currentCell[1]=19;
 				}else{
 					currentCell[1]--;
 				}
 			}else if(event.key=="right"){
-				if(currentCell[1]==9){
+				if(currentCell[1]==19){
 					currentCell[1]=0;
 				}else{
 				currentCell[1]++;
 				}
 			}
-			activeCell();
-		}
+	activeCell();
+}
 
-		var destination = Point.random() * view.size;
-		function onFrame(event){
-			for(var i = 0; i<circles.length; i++){
-				circles[i].fillColor.hue+=3;
-				circles[i].scale(0.95);
-				circles[i].rotate(3);
-				if(circles[i].area < 3){
-   				circles[i].remove();
-			    circles.splice(i, 1);
-			  }
-			}
+function onFrame(event){
+	for(var i = 0; i<circles.length; i++){
+		circles[i].fillColor.hue+=3;
+		circles[i].scale(0.95);
+		circles[i].rotate(3);
+		if(circles[i].area < 3){
+			circles[i].remove();
+		    circles.splice(i, 1);
 		}
+	}
+}
 
-		var kLetters = document.querySelectorAll(".letter");
-			for(var i=0;i<kLetters.length;i++){
-				kLetters[i].addEventListener("click", fillTableCell);
-		}
+var kLetters = document.querySelectorAll(".letter");
+	for(var i=0;i<kLetters.length;i++){
+			kLetters[i].addEventListener("click", fillTableCell);
+}
 
 var soundTable = document.querySelectorAll(".sound-table .line");
 	
 function fillTableCell(){
 	removeActive();
 	soundTable[currentCell[0]].children[currentCell[1]].children[0].textContent = set[currentSetI] + this.children[0].textContent;
-	if(currentCell[1]==9 && currentCell[0]<3){
+	if(currentCell[1]==19 && currentCell[0]<3){
 		currentCell[0]++;
 		currentCell[1]=0;
-	}else if(currentCell[1]==9 && currentCell[0]==3){
+	}else if(currentCell[1]==19 && currentCell[0]==3){
 		currentCell[0]==0;
 		currentCell[1]==1;
 	}else{
@@ -302,37 +299,48 @@ function fillTableCell(){
 
 var currSound = 0 ;
 
-function loop(){
+var loop = function(){
+	nowPlaying();
 	var sound1 = new Howl({
-  		src: ["sounds/" + soundTable[0].children[currSound].textContent[0] + "/"+ keySounds[soundTable[0].children[currSound].textContent[1]]]
+  		src: ["sounds/" + soundTable[0].children[currSound].textContent[0] + "/"+ keySounds[soundTable[0].children[currSound].textContent[1]]],
 		});
-var sound2 = new Howl({
+	var sound2 = new Howl({
   		src: ["sounds/" + soundTable[1].children[currSound].textContent[0] + "/"+ keySounds[soundTable[1].children[currSound].textContent[1]]]
 		});
-var sound3 = new Howl({
-  		src: ["sounds/" + soundTable[2].children[currSound].textContent[0] + "/"+ keySounds[soundTable[2].children[currSound].textContent[1]] ]
+	var sound3 = new Howl({
+  		src: ["sounds/" + soundTable[2].children[currSound].textContent[0] + "/"+ keySounds[soundTable[2].children[currSound].textContent[1]]]
 		});
-var sound4 = new Howl({
-  		src: ["sounds/" + soundTable[3].children[currSound].textContent[0] + "/"+ keySounds[soundTable[3].children[currSound].textContent[1]] ]
+	var sound4 = new Howl({
+  		src: ["sounds/" + soundTable[3].children[currSound].textContent[0] + "/"+ keySounds[soundTable[3].children[currSound].textContent[1]]]
 		});
-	 "sounds/" + soundTable[1].children[currSound].textContent[0] + "/"+ keySounds[soundTable[1].children[currSound].textContent[1]] 
 	sound1.play();
 	sound2.play();
 	sound3.play();
 	sound4.play();
-	if(currSound==9){
+	function nowPlaying(){
+		soundTable[0].children[currSound].classList.toggle("nowP");
+		soundTable[1].children[currSound].classList.toggle("nowP");
+		soundTable[2].children[currSound].classList.toggle("nowP");
+		soundTable[3].children[currSound].classList.toggle("nowP");
+	}
+	if(currSound==19){
 		currSound=0;
 	}else{
 		currSound++;
 	}
 };
-  	var startB = document.querySelector("#start");
-	var stopB = document.querySelector("#stop");
-	var hideB = document.querySelector("#hide");
-	hideB.addEventListener("click",function(){
-		document.querySelector(".box").style.display = "none";
-		window.setInterval(loop, 500);
-		activeCell();
-	})
-
-	
+var startB = document.querySelector("#start");
+var stopB = document.querySelector("#cancel");
+var hideB = document.querySelector("#hide");
+hideB.addEventListener("click",function(){
+	document.querySelector(".box").style.display = "none";
+	activeCell();
+})
+var interv = function(){
+	stopI = window.setInterval(loop, 500);
+}
+var stopInt = function(num){
+	return window.clearInterval(num)
+}
+startB.addEventListener("click", interv);
+stopB.addEventListener("click", stopInt(stopI));
